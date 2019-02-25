69c955de11e8e0e8d7c1332fe7a20d2483f2927f
smart <zeynel.sevgili@gmail.com>
2019-02-25 08:46:12 +0300

    Initial Commit

---
+++ Dicle Fırat Dental Grup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicle Fırat Dental Grup
+//
+//  Created by Demo on 25.02.2019.
+//  Copyright © 2019 Demo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
